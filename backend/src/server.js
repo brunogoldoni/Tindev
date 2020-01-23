ff0417ab083eb8c3ca57b4d73fd8766b87457531
bgoldoni <bgoldoni@orion.tech>
2020-01-23 14:33:06 -0300

    Backend - Creating application base

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,18 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const routes = require('./routes');
 
 const server = express();
 
-server.get('/', (req, res) => {
-  return res.json({ message: `Olá, ${req.query.name}` });
+mongoose.connect('mongodb://bruno:omnistack@heineken-shard-00-00-mlhas.mongodb.net:27017,heineken-shard-00-01-mlhas.mongodb.net:27017,heineken-shard-00-02-mlhas.mongodb.net:27017/week8?ssl=true&replicaSet=Heineken-shard-0&authSource=admin&retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
 });
+
+server.use(cors());
+server.use(express.json());
+server.use(routes);
 
 server.listen(3333);
